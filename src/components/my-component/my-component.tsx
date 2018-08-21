d33a0382b8161d9262c08a48bc9ff5153653c5a5
Alessandro Genova <alessandro.genova@kitware.com>
2018-08-21 10:26:11 -0400

    Fix plotting of the quaternary shells

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -6,8 +6,8 @@ import { Ingredient } from '../../types';
 
 import { viridis } from '../../utils/colors';
 
-// import sampleData from '../../sample-data/FeMnNiZn.json';
-import { generateRandom } from '../../sample-data/random';
+import sampleData from '../../sample-data/FeMnNiZn.json';
+// import { generateRandom } from '../../sample-data/random';
 
 @Component({
   tag: 'my-component',
@@ -21,7 +21,7 @@ export class MyComponent {
   fullData: any[];
 
   plotsContainer: HTMLElement;
-  svgElements: HTMLElement[] = [null, null, null, null, null];
+  svgElement: HTMLElement;
 
   ingredients: Ingredient[];
 
@@ -34,55 +34,58 @@ export class MyComponent {
 
     let A: Ingredient = {
       key: 'Zn',
-      label: 'A',
+      label: 'Zn',
       extent: extent,
       spacing: spacing
     };
     let B: Ingredient = {
       key: 'Fe',
-      label: 'B',
+      label: 'Fe',
       extent: extent,
       spacing: spacing
     };
     let C: Ingredient = {
       key: 'Mn',
-      label: 'C',
+      label: 'Mn',
       extent: extent,
       spacing: spacing
     };
     let D: Ingredient = {
       key: 'Ni',
-      label: 'D',
+      label: 'Ni',
       extent: extent,
       spacing: spacing
     }
 
     this.ingredients = [A, B, C, D];
-    this.fullData = generateRandom(
-      [A.label, B.label, C.label, D.label],
-      ['Jmin.mAcm2'],
-      extent,
-      spacing
-    )
-    // this.fullData = sampleData;
+    // this.fullData = generateRandom(
+    //   [A.key, B.key, C.key, D.key],
+    //   ['Jmin.mAcm2'],
+    //   extent,
+    //   spacing
+    // )
+    this.fullData = sampleData;
 
   }
 
   componentDidLoad() {
+    const spacing = this.ingredients[0].spacing;
+    let start = 50;
+    const edgeUnit = 350;
     for (let i = 0; i < 3; ++i) {
-      this.plotShell(this.svgElements[i], i);
+      let constValue = i * spacing;
+      let edge = Math.floor(edgeUnit * (1 - constValue * 4));
+      this.plotShell(this.svgElement, constValue, edge, start);
+      start += 2 * edge + 0.2 * edgeUnit;
     }
   }
 
-  plotShell(svg: HTMLElement, i: number) {
+  plotShell(svg: HTMLElement, constValue: number, edge: number, shift: number) {
     const n = 4;
-    const spacing = this.ingredients[0].spacing;
-    const constValue = i * spacing;
     let plots: TernaryPlot[] = [];
-    const edge = Math.floor(350 * (1 - constValue));
     for (let i = 0; i < n; ++i) {
       let plot = new TernaryPlot(`lulzi${i}`, svg, i % 2 !== 0);
-      let left = i * (edge / 2) + 50;
+      let left = shift + i * (edge / 2);
       let right = left + edge;
       plot.setSize(left, right);
       plots.push(plot);
@@ -93,38 +96,50 @@ export class MyComponent {
     let B = {...this.ingredients[1]};
     let C = {...this.ingredients[2]};
     let D = {...this.ingredients[3]};
-    A.extent = [A.extent[0], A.extent[1] - constValue];
-    B.extent = [B.extent[0], B.extent[1] - constValue];
-    C.extent = [C.extent[0], C.extent[1] - constValue];
-    D.extent = [D.extent[0], D.extent[1] - constValue];
+    A.extent = [A.extent[0] + constValue, A.extent[1] - 3 * constValue];
+    B.extent = [B.extent[0] + constValue, B.extent[1] - 3 * constValue];
+    C.extent = [C.extent[0] + constValue, C.extent[1] - 3 * constValue];
+    D.extent = [D.extent[0] + constValue, D.extent[1] - 3 * constValue];
 
-    let mapRange: [number, number] = [0, 1];
-    // let mapRange: [number, number] = [-40, 0];
+    // let mapRange: [number, number] = [0, 1];
+    let mapRange: [number, number] = [-40, 0];
     let eps = 1e-6;
 
     slicedData = this.fullData
-      .filter((val) => Math.abs(val[D.key] - constValue) < eps);
+      .filter((val) => Math.abs(val[D.key] - constValue) < eps)
+      .filter((val) => val[A.key] >= constValue)
+      .filter((val) => val[B.key] >= constValue)
+      .filter((val) => val[C.key] >= constValue);
     plots[0].setColorMap(viridis, mapRange);
     plots[0].setAxes(A, B, C, D);
     plots[0].setData(slicedData);
 
     slicedData = this.fullData
-      .filter((val) => val[A.key] === constValue)
-      .filter((val) => val[D.key] !== 0);
+      .filter((val) => Math.abs(val[A.key] - constValue) < eps)
+      .filter((val) => Math.abs(val[D.key] - constValue) > eps)
+      .filter((val) => val[B.key] >= constValue)
+      .filter((val) => val[C.key] >= constValue)
+      .filter((val) => val[D.key] >= constValue);
     plots[1].setColorMap(viridis, mapRange);
     plots[1].setAxes(C, D, B, A);
     plots[1].setData(slicedData);
 
     slicedData = this.fullData
-      .filter((val) => val[C.key] === constValue)
-      .filter((val) => val[A.key] !== 0);
+      .filter((val) => Math.abs(val[C.key] - constValue) < eps)
+      .filter((val) => Math.abs(val[A.key] - constValue) > eps)
+      .filter((val) => val[A.key] >= constValue)
+      .filter((val) => val[B.key] >= constValue)
+      .filter((val) => val[D.key] >= constValue);
     plots[2].setColorMap(viridis, mapRange);
     plots[2].setAxes(B, A, D, C);
     plots[2].setData(slicedData);
 
     slicedData = this.fullData
-      .filter((val) => val[B.key] === constValue)
-      .filter((val) => val[C.key] !== 0);
+      .filter((val) => Math.abs(val[B.key] - constValue) < eps)
+      .filter((val) => Math.abs(val[C.key] - constValue) > eps)
+      .filter((val) => val[A.key] >= constValue)
+      .filter((val) => val[C.key] >= constValue)
+      .filter((val) => val[D.key] >= constValue);
     plots[3].setColorMap(viridis, mapRange);
     plots[3].setAxes(D, C, A, B);
     plots[3].setData(slicedData);
@@ -135,21 +150,7 @@ export class MyComponent {
   render() {
     return (
       <div class="fill" ref={(ref) => {this.plotsContainer = ref;}}>
-        <div class="half-fill-v">
-          <svg class="fill" ref={(ref) => {this.svgElements[0] = ref;}}></svg>
-        </div>
-        <div class="half-fill-v">
-          <svg class="fill" ref={(ref) => {this.svgElements[1] = ref;}}></svg>
-        </div>
-        <div class="half-fill-v">
-          <svg class="fill" ref={(ref) => {this.svgElements[2] = ref;}}></svg>
-        </div>
-        {/* <div class="half-fill-v">
-          <svg class="fill" ref={(ref) => {this.svgElements[3] = ref;}}></svg>
-        </div>
-        <div class="half-fill-v">
-          <svg class="fill" ref={(ref) => {this.svgElements[4] = ref;}}></svg>
-        </div> */}
+        <svg class="fill" ref={(ref) => {this.svgElement = ref;}}></svg>
       </div>
     );
   }
