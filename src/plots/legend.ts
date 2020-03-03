7b3fbabb84b03b521e4e62dc471529030563dc2d
Alessandro Genova <alessandro.genova@kitware.com>
2020-03-03 15:39:02 -0500

    Add a category legend visualization

--- src/plots/legend.ts
+++ src/plots/legend.ts
@@ -2,12 +2,10 @@ import { Vec2, Vec3 } from "../types";
 import { linearScale, createColorMap, Scale } from '@colormap/core';
 import { select } from 'd3-selection';
 
-export class ColorMapLegend {
-  range: Vec2;
+export class BaseLegend {
   colors: Vec3[];
   direction: 'horizontal' | 'vertical';
-  digits: number;
-  inverted: boolean;
+  container: HTMLDivElement
   image: HTMLImageElement;
   canvas: HTMLCanvasElement;
   context: CanvasRenderingContext2D;
@@ -15,14 +13,14 @@ export class ColorMapLegend {
   nPoints: number;
   fontSize: number = 0.8;
   wrapper: HTMLDivElement;
+  opacity: number;
 
-  constructor(private container: HTMLDivElement) {
-    this.range = [0, 1];
+  constructor(container: HTMLDivElement) {
+    this.container = container;
     this.colors = [[0, 0, 0], [1, 1, 1]];
     this.nPoints = 128;
+    this.opacity = 1;
     this.direction = 'vertical';
-    this.digits = 1;
-    this.inverted = false;
     this.wrapper = document.createElement('div');
     this.wrapper.style.width = '100%';
     this.wrapper.style.height = '100%';
@@ -33,11 +31,6 @@ export class ColorMapLegend {
     this.context = this.canvas.getContext('2d')!;
     this.wrapper.appendChild(this.image);
     this.container.appendChild(this.wrapper);
-    this.draw();
-  }
-
-  setRange(range: Vec2) {
-    this.range = [range[0], range[1]];
   }
 
   setColors(colors: Vec3[]) {
@@ -72,6 +65,29 @@ export class ColorMapLegend {
     }
   }
 
+  setOpacity(opacity: number) {
+    this.opacity = opacity;
+  }
+}
+
+export class ColorMapLegend extends BaseLegend {
+  range: Vec2;
+  digits: number;
+  inverted: boolean;
+
+  constructor(container: HTMLDivElement) {
+    super(container);
+    this.range = [0, 1];
+    this.nPoints = 128;
+    this.digits = 1;
+    this.inverted = false;
+    this.draw();
+  }
+
+  setRange(range: Vec2) {
+    this.range = [range[0], range[1]];
+  }
+
   setDigits(digits: number) {
     this.digits = digits;
   }
@@ -104,7 +120,7 @@ export class ColorMapLegend {
     const colorMap = createColorMap(this.colors, scale);
 
     for (let i = 0; i < this.nPoints; ++i) {
-      colorMap(i).concat(1)
+      colorMap(i).concat(this.opacity)
         .map(v => v * 255)
         .forEach((c, j) => {
           this.imageData.data[i * 4 + j] = c;
@@ -140,6 +156,99 @@ export class ColorMapLegend {
       }
       selection
         .text((d: number) => d.toFixed(this.digits))
+        .style('position', 'absolute')
+        .style('font-size', `${this.fontSize}rem`)
+        .style('bottom', bottom)
+        .style('transform', transform)
+        .style('left', left);
+    }
+
+    // Enter
+    setupSpans(spans.enter().append('span'));
+    // Update
+    setupSpans(spans);
+    // Remove
+    spans.exit().remove();
+  }
+}
+
+export class CategoryLegend extends BaseLegend {
+  labels: string[];
+
+  constructor(container: HTMLDivElement) {
+    super(container);
+    this.nPoints = 128;
+    this.labels = ['0', '1'];
+    this.draw();
+  }
+
+  setLabels(labels: string[]) {
+    this.labels = [...labels];
+  }
+
+  draw() {
+    this.drawMap();
+    this.drawTicks();
+  }
+
+  private drawMap() {
+    const nLabels = this.labels.length;
+    const nColors = this.colors.length;
+    const nCategories = Math.max(nLabels, nColors);
+
+    for (let i = 0; i < this.nPoints; ++i) {
+      const iCategory = Math.floor(nCategories * i / this.nPoints);
+      let label = '';
+      let color = [1, 1, 1];
+
+      if (iCategory < nLabels) {
+        label = this.labels[iCategory];
+      }
+      label;
+
+      if (iCategory < nColors) {
+        color = [...this.colors[iCategory]];
+      }
+
+      color.concat(this.opacity)
+        .map(v => v * 255)
+        .forEach((c, j) => {
+          this.imageData.data[i * 4 + j] = c;
+        });
+    }
+
+    this.context.putImageData(this.imageData, 0, 0);
+    this.image.src = this.canvas.toDataURL();
+  }
+
+  private drawTicks() {
+    const nLabels = this.labels.length;
+    const nColors = this.colors.length;
+    const nCategories = Math.max(nLabels, nColors);
+
+    const nTicks = nCategories;
+    const ticks : string[] = [];
+    for (let i = 0; i < nTicks; ++i) {
+      ticks.push(this.labels[i] || '');
+    }
+
+    const spans = select(this.wrapper).selectAll('span').data(ticks);
+
+    const setupSpans = (selection: any) => {
+      let bottom : any;
+      let left: any;
+      let transform: string;
+      if (this.direction === 'horizontal') {
+        left = (_d: number, i: number) => `${(i + 0.5) / (nTicks) * 100}%`;
+        bottom = '1.5rem';
+        transform = 'translateX(-50%)';
+      } else {
+        bottom = (_d: number, i: number) => `${(1 - (i + 0.5) / (nTicks)) * 100}%`;
+        left = '1.5rem';
+        transform = 'translateY(50%)';
+      }
+      selection
+        .text((d: string) => d)
         .style('position', 'absolute')
         .style('font-size', `${this.fontSize}rem`)
         .style('bottom', bottom)
